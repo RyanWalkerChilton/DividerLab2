ce44f6a4edcdbfd7177913383278be27ac3febb2
Varshini Jeevaraj <32648876+varshj@users.noreply.github.com>
2017-10-11 16:50:51 -0500

    Add files via upload

--- divider.vhd
+++ divider.vhd
@@ -1,6 +1,8 @@
 library IEEE;
+library STD;
 use IEEE.std_logic_1164.all;
 use WORK.divider_const.all;
+use IEEE.numeric_std.all;
 --Additional standard or custom libraries go here
 entity divider is
 port(
@@ -16,10 +18,13 @@ remainder : out std_logic_vector (DIVISOR_WIDTH - 1 downto 0);
 overflow : out std_logic
 );
 end entity divider;
+
+
 architecture structural_combinational of divider is
+
 component comparator is
 generic(
-DATA_WIDTH : natural := 4
+DATA_WIDTH : natural := 16
 );
 port(
 --Inputs
@@ -30,26 +35,57 @@ DOUT : out std_logic_vector (DATA_WIDTH - 1 downto 0);
 isGreaterEq : out std_logic
 );
 end component comparator;
+
+
+signal divid :std_logic_vector (DIVIDEND_WIDTH - 1 downto 0) := dividend;
+--signal dividtemp : std_logic_vector (DIVIDEND_WIDTH - 1 downto 0) ;
+signal divis :std_logic_vector (DIVISOR_WIDTH - 1 downto 0) := divisor;
+signal remain :std_logic_vector (DIVISOR_WIDTH - 1 downto 0);
+signal quot :std_logic_vector (DIVIDEND_WIDTH - 1 downto 0);
+signal flag :std_logic;
+signal concatdividend : std_logic_vector(DIVIDEND_WIDTH-1 downto 0);
+signal newconcatdividend : std_logic_vector(DIVIDEND_WIDTH-1 downto 0);
+constant ZERO : std_logic_vector(DIVIDEND_WIDTH-1 downto 0) := (others => '0');
 begin
-G1: FOR i in 0 downto ((DIVIDEND_WIDTH-DIVISOR_WIDTH)-1) GENERATE
+--dividtemp<=shift_right(dividend,DIVIDEND_WIDTH-1);
+newconcatdividend<= ZERO(DIVIDEND_WIDTH-1 downto 1) & divid(DIVIDEND_WIDTH-1);
 
-	Condition_First: if i = 0 GENERATE begin
-		First: comparator
-			port map(dividend(DIVIDEND_WIDTH-1 downto DIVIDEND_WIDTH-6)<=DINL,divisor(DIVIDEND_WIDTH-1 downto DIVIDEND_WIDTH-5)<=DINR, --Remainder Signal
-			)
-	end GENERATE;
-	
-		Condition_Last: if i = ((DIVIDEND_WIDTH-DIVISOR_WIDTH)-1) GENERATE begin
-		Last: comparator
-			port map()
-	end GENERATE;
-	
-		Condition_Middle: if i > 0 AND i<((DIVIDEND_WIDTH-DIVISOR_WIDTH)-1) GENERATE begin
-		Middle: comparator
-			port map()
+G1: FOR i in 0 to (DIVIDEND_WIDTH-1) GENERATE    ---DIVISOR_WIDTH+1-1
+
+Condition_First: if i = 0 GENERATE begin
+
+	First: comparator
+   port map(
+		DINL=> newconcatdividend,   
+	        DINR=>divis,     --(DIVISOR_WIDTH-1 downto 0),
+                DOUT=>remain,     --(DIVISOR_WIDTH - 1 downto 0)
+                isGreaterEq => quot(DIVIDEND_WIDTH-1)
+			);
 	end GENERATE;
 
 
+Condition_Middle: if i > 0 AND i<=(DIVIDEND_WIDTH-1) GENERATE begin
+concatdividend <= remain & divid(DIVIDEND_WIDTH-1-i);
+     Middle: comparator
+   port map(
+                DINL=> concatdividend,     
+	        DINR=> divis,
+                DOUT=> remain,
+                isGreaterEq => quot(DIVIDEND_WIDTH-1-i)
+			);
+	end GENERATE;
 
-END GENERATE;
+		
+		
+--Condition_Last: if i = (DIVIDEND_WIDTH-DIVISOR_WIDTH) GENERATE begin
+--		Last: comparator
+--    port map(
+-- );
+	--end GENERATE;
+
+quotient<=quot;
+remainder<=remain;
+
+END generate;
+
 end architecture structural_combinational;
